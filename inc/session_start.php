06e852d080ca6b5ebca984c65f445dd13a7a6b93
34656 <paulibfg@gmail.com>
2024-09-23 19:23:33 +0200

    features usuarios

---
+++ inc/session_start.php
@@ -0,0 +1,3 @@
+<?php
+ session_name('iventario');
+ session_start();
---
+++ vistas/home.php
@@ -0,0 +1,6 @@
+<section class="section is-medium">
+  <h1 class="title">Bienvenido al sistema de inventario, <?php echo $_SESSION['nombre'] ?></h1>
+  <h2 class="subtitle">
+    Esta es una aplicacion de gestion de productos e inventarios desarrollada con HTML, PHP y JavaScript.
+  </h2>
+</section>
